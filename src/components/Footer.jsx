023ee4fcd9accd30497f7bceef34562a12236fa2
pranavpallattu <pranavpspallattu@gmail.com>
2024-12-08 09:59:24 +0530

    final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -58,13 +58,13 @@ function Footer() {
             </div>
           </div>
         </div>
-        <div className="container d-md-flex md-justify-content-between my-5">
+        <div className="container d-lg-flex  justify-content-between my-5">
           <div>
-          <FontAwesomeIcon className="me-5  ms-lg-0 ms-0 fs-5" icon={faFacebook} />
-          <FontAwesomeIcon className="me-5 ms-lg-0 ms- 4fs-5" icon={faX} />
-          <FontAwesomeIcon className="me-5 ms-lg-0 ms-4 fs-5" icon={faYoutube} />
-          <FontAwesomeIcon className="me-5 ms-lg-0 ms-4 fs-5" icon={faLinkedin} />
-          <FontAwesomeIcon className="me-5 ms-lg-0 ms-4 fs-5" icon={faInstagram} />
+          <FontAwesomeIcon className=" me-lg-5 me-3 ms-lg-0 ms-0 fs-5" icon={faFacebook} />
+          <FontAwesomeIcon className=" me-lg-5 me-3 ms-lg-0 ms-3 fs-5" icon={faX} />
+          <FontAwesomeIcon className=" me-lg-5 me-3 ms-lg-0 ms-3 fs-5" icon={faYoutube} />
+          <FontAwesomeIcon className=" me-lg-5 me-3 ms-lg-0 ms-3 fs-5" icon={faLinkedin} />
+          <FontAwesomeIcon className=" me-lg-5 me-3 ms-lg-0 ms-3 fs-5" icon={faInstagram} />
           </div>
           <div className="d-md-flex mt-lg-0 mt-5">
             <p className="me-3"><FontAwesomeIcon icon={faGlobe} />
@@ -74,13 +74,13 @@ function Footer() {
           </div>
           <div className="container">
             <button><img src="https://en.logodownload.org/wp-content/uploads/2019/06/get-it-on-google-play-badge.png" style={{height:'40px'}} alt="" /></button>
-            <button className="ms-2"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/2560px-Download_on_the_App_Store_Badge.svg.png" style={{height:'40px'}} alt="" /></button>
+            <button className="ms-lg-2 ms-0"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/2560px-Download_on_the_App_Store_Badge.svg.png" style={{height:'40px'}} alt="" /></button>
           </div>
-          <div className="container d-md-flex justify-content-between">
+          <div className="container d-lg-flex justify-content-between">
             <div>
               <p className="mt-5 fst-normal">© 2024 Uber Technologies Inc.</p>
             </div>
-            <div className="mt-lg-0 mt-5">
+            <div className=" mt-4 d-none d-lg-block">
               <button className="btn text-light">Privacy</button>
               <button className="btn text-light ms-3">Accessibility</button>
               <button  className="btn text-light ms-3">Terms</button>
